08bcb1a3651d8e52a33cd89f58f96317c47a2e4d
Stas_Krasnagir <stas220693@gmail.com>
2020-10-17 19:23:12 +0300

    17/10/20 19.23

--- string.py
+++ string.py
@@ -1,3 +1,4 @@
+# length() - размер строки
 A = "Hello World"
 def len_string (A):
     i = 0
@@ -5,3 +6,25 @@ def len_string (A):
         i += 1
     return i
 print(len_string(A))
+
+# charAt() - первое вхождение элемента
+# я хотел глянуть как рабоат метод ".find" для проверки работы функции
+string1 = str(input("Type something: "))
+serth_s1 = str(input("Searching symbol: "))
+res1 = string1.find(serth_s1)
+print(res1)
+
+
+string = str(input("Type something: "))
+serth_s = str(input("Searching symbol: "))
+def charAt(string, serth_s):
+    count = 0
+    for value in string:
+        if value ==serth_s:
+            return count 
+        count+=1
+    return -1
+res = charAt(string, serth_s)
+print(res)
+
+
