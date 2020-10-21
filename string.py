a76d5db8259f62e3c9ba278a9ef3367e722bd0d6
Stas_Krasnagir <stas220693@gmail.com>
2020-10-21 15:44:59 +0300

    21-10-2020

--- string.py
+++ string.py
@@ -1,43 +1,35 @@
-# length() - размер строки
-A = "Hello World"
-def len_string (A):
+def len_string (string):
     i = 0
-    while A[i:]:
+    while string[i:]:
         i += 1
     return i
-print(len_string(A))
 
-# charAt() - первое вхождение элемента
-# я хотел глянуть как рабоат метод ".find" для проверки работы функции
-string1 = str(input("Type something: "))
-serth_s1 = str(input("Searching symbol: "))
-res1 = string1.find(serth_s1)
-print(res1)
-
-
-string = str(input("Type something: "))
-serth_s = str(input("Searching symbol: "))
-def charAt(string, serth_s):
+def charAt(string, serth_symbal):
     count = 0
     for value in string:
-        if value ==serth_s:
+        if value ==serth_symbal:
             return count 
         count+=1
     return -1
-res = charAt(string, serth_s)
-print(res)
 
-# trim() - удалить все лишние пробелы 
-input_string = input()
-i = 0
-while input_string[i] == ' ':
-    input_string = input_string[1:]
-while input_string[len(input_string)-1] == ' ':
-    input_string = input_string[:-1]
-i = 1
-while i < len(input_string)-1:
-    if input_string[i] == ' ' and input_string[i+1] == ' ':
-        input_string = input_string[:i+1] + input_string[i+2:]
-    else:
-        i += 1
-print(input_string)
+def trim (string):
+    i = 0
+    while string[i] == ' ':
+      string = string[1:]
+    while string[len(string)-1] == ' ':
+        string = string[:-1]
+    i = 1
+    while i < len(string)-1:
+        if string[i] == ' ' and string[i+1] == ' ':
+            string = string[:i+1] + string[i+2:]
+        else:
+            i += 1
+    return(string)
+
+
+string = "  Hello   World  "
+
+print(len_string(string))
+serth_symbal = str(input("Searching symbol: "))
+print(charAt(string, serth_symbal))
+print(trim(string))
