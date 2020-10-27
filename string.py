2cf2143d27d0c92ea300b831dd3921fb237ea7a3
Stas_Krasnagir <stas220693@gmail.com>
2020-10-27 20:17:31 +0300

    27-10-2020

--- string.py
+++ string.py
@@ -12,33 +12,29 @@ def charAt(string, char):
         count += 1
     return -1
 
-#def trim(string):
-#    i = 0
-#    while string[i] == ' ':
-#      string = string[1:]
-#    while string[len(string)-1] == ' ':
-#        string = string[:-1]
-#    i = 1
-#    while i < len(string)-1:
-#        if string[i] == ' ' and string[i+1] == ' ':
-#            string = string[:i+1] + string[i+2:]
-#        else:
-#            i += 1
-#    return string
-
 def trim(string):
     new_string = ""
     for i, ch in enumerate(string):
-        if i == " ":
-            if string[i+1] != " ":
-                new_string = new_string + string[i+1]
+        if ch == " ":
+            if len(string) != i + 1 and string[i + 1] != " ":
+                new_string = new_string + ch
         else:
-            new_string = new_string + string[i]
+            new_string = new_string + ch
     return new_string
 
-string = "  Hello   World  "
+def replace_str(string, old_value, new_value):
+    index_of_old_value_in_string = string.find(old_value)
+    len_of_old_value = len(old_value)
+    new_string = string[ :index_of_old_value_in_string] + new_value + string[index_of_old_value_in_string + len_of_old_value : ]
+    return new_string
+
+string = "  Hello  new  World  "
 
 print(len_string(string))
 char = str(input("Searching symbol: "))
 print(charAt(string, char))
 print(trim(string))
+
+old_value = "new"
+new_value = "wonderful"
+print(replace_str(string, old_value, new_value))
